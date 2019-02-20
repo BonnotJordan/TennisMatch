ccde979c05e36a151ccd8af0a75a272035b53df6
Jordan Bonnot <jojobonnot@gmail.com>
2019-02-20 23:53:56 +0100

    fin (reformat) + tests

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,19 @@
 import java.util.HashMap;
 
 public class Player {
+
     private String name;
     private String numberOfPoints = "0";
     private int numberOfGames = 0;
     private int numberOfSets = 0;
     private HashMap<Integer, Integer> allGamesWin = new HashMap<>();
 
+    //Constructeur
     public Player(String name) {
         this.name = name;
     }
 
+    //getters
     public String getName() {
         return name;
     }
@@ -27,6 +30,11 @@ public class Player {
         return numberOfSets;
     }
 
+    public int getGamesInSet(Integer setNum){
+        return allGamesWin.get(setNum);
+    }
+
+    //setters
     public void setName(String name) {
         this.name = name;
     }
@@ -35,29 +43,30 @@ public class Player {
         this.numberOfPoints = numberOfPoints;
     }
 
+    //jeu gagné
     public void winGame() {
         numberOfGames++;
         numberOfPoints = "0";
     }
 
+    //jeu perdu
     public void looseGame() {
         numberOfPoints = "0";
     }
 
+    //set gagné
     public void winSet() {
         numberOfSets++;
+        //On stocke le nombre de jeu gagnés pour ce set
         allGamesWin.put(allGamesWin.size() + 1,numberOfGames);
         numberOfGames = 0;
     }
 
+    //set perdu
     public void looseSet() {
+        //On stocke le nombre de jeu gagnés pour ce set
         allGamesWin.put(allGamesWin.size() + 1,numberOfGames);
         numberOfGames = 0;
-    }
-
-
-    public int getGamesInSet(Integer setNum){
-        return allGamesWin.get(setNum);
     }
 
 }
